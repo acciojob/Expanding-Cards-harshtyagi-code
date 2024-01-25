9672120c883e4e795ecfdd423276ec75342852b8
Harsh Tyagi <69513043+harshtyagi-code@users.noreply.github.com>
2024-01-25 10:10:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+let panels=document.querySelectorAll(".panel");
+
+// for(let i=0;i<panels.length;i++){
+// 	panels[i].addEventListener("click", onClickToggle)
+// }
+
+function onClickToggle(e) {
+
+	for(let i=0;i<panels.length;i++){
+	
+		panels[i].classList.remove("active")
+	
+	}
+		
+		
+			e.classList.add("active");
+			
+			
+}
